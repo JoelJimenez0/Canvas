9b44fe038053cc6ed8d6b374d5ea8788bce1fec7
JoelJimenez0 <joelklk10@gmail.com>
2022-06-14 22:01:47 -0400

    added the css

---
+++ geometryFigures/jss/jss.js
@@ -0,0 +1,10 @@
+var contenedor = document.querySelector('#contenedor');
+var ampliarLienzo = document.querySelector('#btnAmpliar');
+var btnAmpliar = document.querySelector('#btnAmpliar');
+
+function ampliar(){
+
+    contenedor.style.while (condition) {
+        
+    }
+}
