1e1cd2c1b1c5420f39ee4806f22106d95471b44c
JoelJimenez0 <joelklk10@gmail.com>
2022-06-15 21:39:18 -0400

    base of the game

--- geometryFigures/jss/jss.js
+++ geometryFigures/jss/jss.js
@@ -1,40 +1,62 @@
-var contenedor = document.querySelector('#contenedor');
-var ampliarLienzo = document.querySelector('#lienzo');
-var btnAmpliar = document.querySelector('#btnAmpliar');
+const canvas = document.getElementById("lienzo");
+const ctx = canvas.getContext("2d");
 
-function ampliar(){
+const circle = document.getElementById("circle");
+const oval = document.getElementById("oval");
+const rectangle = document.getElementById("rectangle");
+const square = document.getElementById("square");
+const triangle = document.getElementById("triangle");
 
-    contenedor.style.width = "100%";
-    contenedor.style.height = "100vh";
-    contenedor.style.margin = "0";
 
-    ampliarLienzo.style.width = "100%";
-    ampliarLienzo.style.height = "100vh";
-    ampliarLienzo.style.backgroundSize = "cover";
-    ampliarLienzo.style.backgroundRepeat = "no-repeat";
+circle.addEventListener("click", function(){
+    ctx.beginPath();
+    ctx.arc(80,70,60,0,2*Math.PI);
+    ctx.fillStyle =("blue");
+    ctx.fill();
+});
 
-    btnAmpliar.innerHTML = "Reducir Canvas";
-    btnAmpliar.style.position = "fixed";
-    btnAmpliar.style.top = "10px";
-    btnAmpliar.style.left = "10px";
-    btnAmpliar.style.zIndex = "10";
+rectangle.addEventListener("click", function(){
+    ctx.fillStyle = "blue";
+    //fillRect(x1,y1,x2,y2);
+    ctx.fillRect(420,17,180, 80);
+});
 
-    btnAmpliar.setAttribute("onclick","reducir()");
-    }
-    function reducir(){
+square.addEventListener("click", function(){
+    ctx.fillStyle = "blue";
+    ctx.fillRect(660,20,100,100);
+});
 
-        contenedor.style.width = "1000px";
-        contenedor.style.height = "500px";
-        contenedor.style.margin = "5vh auto";
-    
-        ampliarLienzo.style.width = "1000px";
-        ampliarLienzo.style.height = "500px";
-    
-        btnAmpliar.innerHTML = "Ampliar Canvas";
-        btnAmpliar.style.position = "relative";
-        btnAmpliar.style.top = "0";
-        btnAmpliar.style.left = "0";
-        btnAmpliar.style.zIndex = "0";
-    
-        btnAmpliar.setAttribute("onclick","ampliar()");
-        }
+oval.addEventListener("click", function(){
+    ctx.beginPath();
+    ctx.moveTo(200, 80); 
+    // ctx.bezierCurveTo(cpx1, cpy1, cpx2, cpy2, x2, y2);
+    ctx.bezierCurveTo(200, 0, 250, 0, 250, 80);
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    ctx.fillStyle =("blue");
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(200, 80); 
+    // ctx.bezierCurveTo(cpx1, cpy1, cpx2, cpy2, x2, y2);
+    ctx.bezierCurveTo(200, 160, 250, 160, 250, 80);
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    ctx.fillStyle =("blue");
+    ctx.fill();
+});
+
+triangle.addEventListener("click", function(){
+    ctx.beginPath();
+    // moveTo(x1, y1);
+    ctx.moveTo(850, 150);
+    // lineTo(x2, y2);
+    ctx.lineTo(960, 150);
+    ctx.lineTo(900, 20);
+    ctx.lineTo(852, 150);
+
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    ctx.fillStyle =("blue");
+    ctx.fill();
+});
