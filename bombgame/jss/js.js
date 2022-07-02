1b0f89f0df7be5313ace1aa286325fe32af25b8b
JoelJimenez0 <joelklk10@gmail.com>
2022-07-01 21:22:34 -0400

    The bomb does not repeat itself, the written letters change its color and the StartBtn dissappear when you click it

--- bombgame/jss/js.js
+++ bombgame/jss/js.js
@@ -2,7 +2,6 @@ const canvas = document.getElementById("lienzo");
 const ctx = canvas.getContext("2d");
 const typingT = document.getElementById("typing");
 const startBtn = document.getElementById("start");
-const bombImg = document.getElementById("bomb");
 const container = document.getElementById("main-container");
 
 const pharagraphs = [
@@ -10,7 +9,7 @@ const pharagraphs = [
     'Usa has the upper hand with his multiple',
     'Nukes and military forces.',
     'Will the japanese people be able to',
-    'win this war even though they seem to be',
+    'win this war, even though they seem to be',
     'weaker? or will they be vanished along',
     'with the USA nukes?',
     'Will the japaneses become victorious',
@@ -20,49 +19,44 @@ const pharagraphs = [
 
  ];
 
+ bomb = new Image();
+ bomb.src = 'img/bomb.png';
+
+ function drawAll(){
+    ctx.drawImage(bomb, bombYposition, bombXposition, 100, 100);
+ }
+
  let bombXposition = 240;
  let bombYposition = -100;
  let bombDroppingFrecuency = 500;
  
  function bombO(){
+    console.log;
      ctx.drawImage(bomb, bombXposition, bombYposition, 100,100);
-     bomb.onload = () => {
-        clear();
-        bomb.style.display = "none";
-      }
-      
-      function drawImage(){
-        ctx.drawImage(bomb, 0, 0);
-      }
-      
-    function clearCanvas(){
-        console.log("Clear")
-        ctx.clearRect(0, 0, 600, 350);
-        drawImage();
-      }
 }
 
  ctx.save();
  function bombDroppingMovement() {
      let bombTime = setInterval(frame, bombDroppingFrecuency);
      function frame() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
          bombO();
          if (bombYposition >= 250) {
-             clearInterval(bombTime);
+            clearInterval(bombTime);
             alert("You've lost");
          } 
          else {
-             bombYposition = bombYposition+ 10;
+             bombYposition = bombYposition+ 7;
          }
      }
  }
- const start = () =>{
+ const start = () =>{   
 
     bombDroppingMovement(); 
     typingProcess();
     function typingProcess (){
         typingT.innerText = "";
-        startBtn.classList.add("hidden");
+        startBtn.style.display = "none";
 
     const text = pharagraphs[parseInt(Math.random() * pharagraphs.length)];
 
@@ -80,7 +74,7 @@ const pharagraphs = [
     const keydown = ({ key })=>{
         if (key === currentLetter.innerText){
             currentLetter.classList.remove("current-letter");
-            currentLetter.classList.add("done");
+            currentLetter.style.color = "blue";
             currentLetter = characters[++cursorIndex];
             bombYposition = bombYposition -3;
         }
